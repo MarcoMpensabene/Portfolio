7c3c9d7e8ebd5bf3071b9c5416d2125710e518fa
MarcoMpensabene <marcompensabene@gmail.com>
2024-10-03 14:00:52 +0200

    added axios for project api , added pagination and authrization code to the script , used a simple html table for now

--- src/components/Main/MainContent.vue
+++ src/components/Main/MainContent.vue
@@ -1,6 +1,44 @@
 <script>
+export default {
+  data() {
+    return {
+      repos: [],
+      page: 1, // Track the current page
+      perPage: 30 // Number of repos per page (default is 30)
+    };
+  },
+  mounted() {
+    this.fetchAllRepos();
+  },
+  methods: {
+    async fetchAllRepos() {
+      let moreRepos = true;
+      while (moreRepos) {
+        try {
+          const response = await fetch(`https://api.github.com/users/MarcoMpensabene/repos?per_page=${this.perPage}&page=${this.page}`, {
+            headers: {
+              Authorization: ``
+            }
+          });
+          const repos = await response.json();
+
+          if (repos.length > 0) {
+            this.repos = [...this.repos, ...repos]; // Append the new repos
+            this.page++; // Move to the next page
+          } else {
+            moreRepos = false; // Stop when no more repos are returned
+          }
+        } catch (error) {
+          console.error('Errore nel recupero dei repository:', error);
+          moreRepos = false;
+        }
+      }
+    }
+  }
+};
 
 </script>
+
 
 <template>
   <div class="main-wrapper">
@@ -11,26 +49,39 @@
     <div class="github">
       Here i will post all my GitHubProject and information abount me .
     </div>
+    <h2>I miei progetti GitHub</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Nome Progetto</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="repo in repos" :key="repo.id">
+          <td>{{ repo.name }}</td>
+          <td><a :href="repo.html_url" target="_blank">Visita</a></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <style scoped lang="scss">
 div .main-wrapper {
   height: 100%;
-  background-image: url("https://img.freepik.com/free-photo/3d-render-low-poly-plexus-design-network-communications_1048-14542.jpg?t=st=1727704513~exp=1727708113~hmac=060f6f5fea6f233d7f24601fa18d3fb329d9cb6dcb9a853a5b7f629a41e0cf4a&w=996");
-  background-repeat: no-repeat;
-  background-size: cover;
+  background-color: gainsboro;
 }
 
 div .description {
   text-align: center;
   margin-bottom: 3rem;
   font-size: larger;
-  color: azure;
+  color: black;
 }
 
 div .github {
   text-align: center;
-  color: azure;
+  color: black;
 }
 </style>
